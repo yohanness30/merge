74f5a3d87e35d036a6950067df8b9b6864d13f7a
Yohannes <yohannes3094@gmail.com>
2024-04-12 22:22:22 +0300

    ver-1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const buttonOne = document.querySelector('.b1');
 const buttonTwo = document.querySelector('.b2');
 const buttonThree = document.querySelector('.b3');
 
-addEventListener('click', () => {
+buttonOne.addEventListener('click', () => {
   if(buttonOne.innerHTML === 'Subscribe'){
     buttonOne.innerHTML = 'Subscribed';
     buttonOne.classList.add('show');
@@ -11,21 +11,21 @@ addEventListener('click', () => {
     buttonOne.classList.remove('show');
   }
 })
-addEventListener('click', () => {
-  if(buttonOne.innerHTML === 'Follow'){
-    buttonOne.innerHTML = 'Followed';
-    buttonOne.classList.add('show');
+buttonTwo.addEventListener('click', () => {
+  if(buttonTwo.innerHTML === 'Follow'){
+    buttonTwo.innerHTML = 'Followed';
+    buttonTwo.classList.add('show');
   } else {
-    buttonOne.innerHTML = 'Follow';
-    buttonOne.classList.remove('show');
+    buttonTwo.innerHTML = 'Follow';
+    buttonTwo.classList.remove('show');
   }
 })
-addEventListener('click', () => {
-  if(buttonOne.innerHTML === 'Donate'){
-    buttonOne.innerHTML = 'Donated';
-    buttonOne.classList.add('show');
+buttonThree.addEventListener('click', () => {
+  if(buttonThree.innerHTML === 'Donate'){
+    buttonThree.innerHTML = 'Donated';
+    buttonThree.classList.add('show');
   } else {
-    buttonOne.innerHTML = 'Donate';
-    buttonOne.classList.remove('show');
+    buttonThree.innerHTML = 'Donate';
+    buttonThree.classList.remove('show');
   }
 })
